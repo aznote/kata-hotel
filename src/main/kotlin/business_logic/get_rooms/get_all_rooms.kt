614dd8539e83ae760b4df3427afad059d5222583
Suxue LI <suxue.li@octo.com>
2021-11-26 15:49:14 +0100

    wip get all rooms

--- src/main/kotlin/business_logic/get_rooms/get_all_rooms.kt
+++ src/main/kotlin/business_logic/get_rooms/get_all_rooms.kt
@@ -6,7 +6,8 @@ class GetAllRooms {
 
     fun execute(): List<Room> {
         val room = Room("1", "101", "1 king size bed - A/C - Wi-Fi - private bathroom - wheelchair accessible", 2)
-        return listOf(room);
+        val otherRoom = Room("1", "102", "1 king size bed - A/C - Wi-Fi - private bathroom - wheelchair accessible", 2)
+        return listOf(room,otherRoom)
     }
 
 }
--- src/test/kotlin/GetAllRoomsTest.kt
+++ src/test/kotlin/GetAllRoomsTest.kt
@@ -7,7 +7,7 @@ internal class GetAllRoomsTest {
     private var getAllRooms: GetAllRooms = GetAllRooms()
 
     @Test
-    fun shouldGet() {
+    fun whenHotelHave1Room_thenShouldReturnTheListWithThisRoom() {
         //Given
 
         //When
@@ -17,6 +17,21 @@ internal class GetAllRoomsTest {
         val expectedRoom =
             Room("1", "101", "1 king size bed - A/C - Wi-Fi - private bathroom - wheelchair accessible", 2)
         val expectedRooms = listOf(expectedRoom)
+
+        assertContentEquals(obtainedRooms, expectedRooms)
+    }
+
+    @Test
+    fun whenHotelHave2Rooms_thenShouldReturnTheListWithTheseRooms() {
+        //When
+        val obtainedRooms = getAllRooms.execute()
+
+        //Then
+        val expectedRoom1 =
+            Room("1", "101", "1 king size bed - A/C - Wi-Fi - private bathroom - wheelchair accessible", 2)
+        val expectedRoom2 =
+            Room("1", "102", "1 king size bed - A/C - Wi-Fi - private bathroom - wheelchair accessible", 2)
+        val expectedRooms = listOf(expectedRoom1, expectedRoom2)
 
         assertContentEquals(obtainedRooms, expectedRooms)
     }
